4bca5b706b9831320d93283649659a736af1a3f5
maryamsartaj <168002522+maryamsartaj@users.noreply.github.com>
2024-05-02 00:27:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var placeTovisit = ["paris", "newyork", "rome", "italy", "china"];
+console.log("original order:", placeTovisit);
+console.log("/n Alphabetical order:", placeTovisit.slice().sort());
+console.log("original order:", placeTovisit);
+console.log("reverse order:", placeTovisit.slice().sort().reverse());
+console.log("original order:", placeTovisit);
+console.log("reverse order changed");
+placeTovisit.reverse();
+console.log(placeTovisit);
+console.log("original order:", placeTovisit.sort());
+console.log(placeTovisit);
+console.log("reverse Alphabetical order changed:", placeTovisit.sort().reverse());
+console.log(placeTovisit);
